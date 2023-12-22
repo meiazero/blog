a12f7392769b7ec3fd80d8b147b23197e8eeea9e
Emanuel Avila <avilaemanueel@gmail.com>
2023-12-22 00:32:17 -0300

    Add banner and feed components to page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,14 @@
+import { Banner } from "@/components/banner";
+import { Feed } from "@/components/feed";
+
 export default function Page() {
   return (
-    <div>
-      <h1 className="text-4xl">Olái mundo!</h1>
-    </div>
+    <>
+      <Banner />
+
+      <section className="mt-6 border-t border-t-zinc-500 px-2 py-4">
+        <Feed />
+      </section>
+    </>
   );
 }
